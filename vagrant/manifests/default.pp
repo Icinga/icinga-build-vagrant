02fe07897e4764658dfa2661f0d7a02e8d1fb633
Markus Frosch <markus@lazyfrosch.de>
2017-01-20 17:48:05 +0100

    Add basic jenkins setup

--- vagrant/manifests/default.pp
+++ vagrant/manifests/default.pp
@@ -1,3 +1,9 @@
 node default {
   hiera_include('classes', [])
+
+  # TODO: replace functionality with profiles
+  # this mimics the behavior of the customer2 module
+  ensure_packages(hiera_array('packages', []))
+  # ensure_resources('package', hiera_hash('package', {}))
+  create_resources('apt::source', hiera_hash('apt::source', {}))
 }
