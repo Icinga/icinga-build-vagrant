49a2ecbab58300b9850aaab282c6fd03e6e6c2e7
Markus Frosch <markus@lazyfrosch.de>
2017-04-18 15:16:18 +0200

    manifests/default: Enable more types that customer2 supports

--- vagrant/manifests/default.pp
+++ vagrant/manifests/default.pp
@@ -6,7 +6,10 @@ node default {
   # ensure_packages(hiera_array('packages', []))
   create_resources('package', hiera_hash('package', {}))
   create_resources('apt::source', hiera_hash('apt::source', {}))
-  create_resources("file", hiera_hash("file", {}))
-  create_resources("cron", hiera_hash("cron", {}))
-  create_resources("apache::custom_config", hiera_hash("apache::custom_config", {}))
+  create_resources('user', hiera_hash('user', {}))
+  create_resources('group', hiera_hash('group', {}))
+  create_resources('exec', hiera_hash('exec', {}))
+  create_resources('file', hiera_hash('file', {}))
+  create_resources('cron', hiera_hash('cron', {}))
+  create_resources('apache::custom_config', hiera_hash('apache::custom_config', {}))
 }
